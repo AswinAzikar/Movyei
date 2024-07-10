4733ccb57ff88e867bf8bf006d25e2983af507cb
AswinAzikar <azwinaswin8@gmail.com>
2024-07-10 13:38:57 +0530

    "Updated launcher icons, refactored HomeScreen widget, and added dependencies to pubspec.yaml"

--- lib/home.dart
+++ lib/home.dart
@@ -12,6 +12,8 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    double screenheight = MediaQuery.of(context).size.width;
+
     double containerWidth = MediaQuery.of(context).size.width < 200
         ? MediaQuery.of(context).size.width
         : 200;
@@ -23,47 +25,108 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: Colors.transparent,
         elevation: 0,
         title: Text(
-          "FLUTFLIX",
+          "MOVIEFLIX",
           style: GoogleFonts.anton(color: Colors.red, fontSize: 40),
         ),
         centerTitle: true,
       ),
       body: SingleChildScrollView(
         physics: const BouncingScrollPhysics(),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Text(
-                "Trending movies",
-                style:
-                    GoogleFonts.abel(textStyle: const TextStyle(fontSize: 24)),
+        child: Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  "Trending movies",
+                  style: GoogleFonts.abel(
+                      textStyle: const TextStyle(fontSize: 24)),
+                ),
               ),
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: CarouselSlider.builder(
+              SizedBox(
+                width: double.infinity,
+                child: CarouselSlider.builder(
+                    itemCount: 10,
+                    options: CarouselOptions(
+                      height: containerHeight,
+                      autoPlay: true,
+                      viewportFraction: 0.55,
+                      autoPlayCurve: Curves.fastOutSlowIn,
+                      enlargeCenterPage: true,
+                      autoPlayAnimationDuration: Duration(seconds: 1),
+                    ),
+                    itemBuilder: (context, index, realIndex) {
+                      return ClipRRect(
+                        borderRadius: BorderRadius.circular(18),
+                        child: Container(
+                          height: containerHeight,
+                          width: containerWidth,
+                          color: Colors.yellow,
+                        ),
+                      );
+                    }),
+              ),
+              SizedBox(height: .1 * screenheight),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  "Top rated movies",
+                  style: GoogleFonts.abel(
+                      textStyle: const TextStyle(fontSize: 24)),
+                ),
+              ),
+              SizedBox(height: .02 * screenheight),
+              SizedBox(
+                height: containerHeight,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  physics: const BouncingScrollPhysics(),
                   itemCount: 10,
-                  options: CarouselOptions(
-                    height: containerHeight,
-                    autoPlay: true,
-                    viewportFraction: 0.55,
-                    autoPlayCurve: Curves.fastOutSlowIn,
-                    autoPlayAnimationDuration: Duration(seconds: 1),
-                  ),
-                  itemBuilder: (context, index, realIndex) {
-                    return ClipRect(
-                    
+                  itemBuilder: (context, index) => Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(18),
                       child: Container(
+                        color: Colors.amber,
                         height: containerHeight,
                         width: containerWidth,
-                        color: Colors.yellow,
                       ),
-                    );
-                  }),
-            )
-          ],
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: .1 * screenheight),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  "Upcomming movies",
+                  style: GoogleFonts.abel(
+                      textStyle: const TextStyle(fontSize: 24)),
+                ),
+              ),
+              SizedBox(height: .02 * screenheight),
+              SizedBox(
+                height: containerHeight,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: 10,
+                  itemBuilder: (context, index) => Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(18),
+                      child: Container(
+                        color: Colors.amber,
+                        height: containerHeight,
+                        width: containerWidth,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:moviyee/Screens/Login_Screen.dart';
+
 import 'package:moviyee/constants.dart';
 
-import 'package:moviyee/controllers/otpVerificationPage.dart';
+
 import 'package:moviyee/controllers/firebase_folder/firebase_options.dart';
 import 'package:moviyee/home.dart';
 
@@ -17,6 +17,9 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   @override
+  
+  
+  
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
