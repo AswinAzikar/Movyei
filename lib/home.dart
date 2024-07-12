8d572ff8bf51ae2c2c93319ee67eea33f596ebb4
AswinAzikar <azwinaswin8@gmail.com>
2024-07-12 16:12:09 +0530

    "Added RefreshIndicator to HomeScreen, modified body layout, and introduced isLoading flag"

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,7 @@ import 'dart:core';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_launcher_icons/android.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:moviyee/Screens/tempscreen.dart';
 import 'package:moviyee/controllers/Api/remote_data_sorce.dart';
@@ -24,6 +25,8 @@ class _HomeScreenState extends State<HomeScreen> {
   int toprated_offset = 1;
   int currentPage = 1;
 
+  bool isLoading = true;
+
   @override
   void initState() {
     super.initState();
@@ -45,90 +48,106 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        controller: scrollController,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              width: screenWidth * 0.1,
-            ),
-            trending.isEmpty
-                ? const Center(child: CircularProgressIndicator())
-                : SizedBox(
-                    child: CarouselSlider.builder(
-                      carouselController: carouselController,
-                      itemCount: trending.length,
-                      options: CarouselOptions(
-                        aspectRatio: 1,
-                        autoPlay: true,
-                        viewportFraction: 0.6,
-                        autoPlayCurve: Curves.fastOutSlowIn,
-                        enlargeCenterPage: true,
-                        autoPlayAnimationDuration: const Duration(seconds: 1),
-                        onPageChanged: (index, reason) {
-                          setState(() {
-                            currentPage = index;
-                          });
-                          if (index == trending.length - 1) {
-                            fetchForUI(offset);
-                          }
-                        },
-                      ),
-                      itemBuilder: (context, index, realIndex) {
-                        final imageUrl =
-                            'https://image.tmdb.org/t/p/w300${trending[index].posterPath}';
-                        return GestureDetector(
-                          onTap: () => Navigator.of(context).push(
-                              MaterialPageRoute(
-                                  builder: (ctx) => InfinitePage())),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Expanded(
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(18),
-                                  child: CachedNetworkImage(
-                                    imageUrl: imageUrl,
-                                    fit: BoxFit.cover,
-                                    placeholder: (context, url) => const Center(
-                                        child: CircularProgressIndicator()),
-                                    errorWidget: (context, url, error) =>
-                                        const Icon(Icons.error),
-                                  ),
-                                ),
-                              ),
-                              SizedBox(
-                                child: Center(
-                                  child: Text(
-                                    trending[index].title!,
-                                    style: const TextStyle(fontSize: 16),
-                                  ),
-                                ),
-                              ),
-                            ],
+      body: RefreshIndicator(
+        onRefresh: () async {
+          setState(() {
+            isLoading = true;
+          });
+          fetchForUI(1);
+        },
+        child: Builder(builder: (context) {
+          if (isLoading) {
+            return Center(child: CircularProgressIndicator());
+          }
+          return SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            controller: scrollController,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: screenWidth * 0.1,
+                ),
+                trending.isEmpty
+                    ? const Center(child: CircularProgressIndicator())
+                    : SizedBox(
+                        child: CarouselSlider.builder(
+                          carouselController: carouselController,
+                          itemCount: trending.length,
+                          options: CarouselOptions(
+                            aspectRatio: 1,
+                            autoPlay: true,
+                            viewportFraction: 0.6,
+                            autoPlayCurve: Curves.fastOutSlowIn,
+                            enlargeCenterPage: true,
+                            autoPlayAnimationDuration:
+                                const Duration(seconds: 1),
+                            onPageChanged: (index, reason) {
+                              setState(() {
+                                currentPage = index;
+                              });
+                              if (index == trending.length - 1) {
+                                fetchForUI(offset);
+                              }
+                            },
                           ),
-                        );
-                      },
-                    ),
-                  ),
-            SizedBox(height: .05 * screenHeight),
-            const HorizontalSliderWIthTitle(
-              title: "Now Playing",
+                          itemBuilder: (context, index, realIndex) {
+                            final imageUrl =
+                                'https://image.tmdb.org/t/p/w300${trending[index].posterPath}';
+                            return GestureDetector(
+                              onTap: () => Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                      builder: (ctx) => InfinitePage())),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Expanded(
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(18),
+                                      child: CachedNetworkImage(
+                                        imageUrl: imageUrl,
+                                        fit: BoxFit.cover,
+                                        placeholder: (context, url) =>
+                                            const Center(
+                                                child:
+                                                    CircularProgressIndicator()),
+                                        errorWidget: (context, url, error) =>
+                                            const Icon(Icons.error),
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    child: Center(
+                                      child: Text(
+                                        trending[index].title!,
+                                        style: const TextStyle(fontSize: 16),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                SizedBox(height: .05 * screenHeight),
+                const HorizontalSliderWIthTitle(
+                  title: "Now Playing",
+                ),
+                SizedBox(height: .05 * screenHeight),
+                const HorizontalSliderWIthTitle(
+                  title: "Top Rated Movies",
+                ),
+                const HorizontalSliderWIthTitle(
+                  title: "Upcoming Movies",
+                ),
+                SizedBox(height: .05 * screenHeight),
+              ],
             ),
-            SizedBox(height: .05 * screenHeight),
-            const HorizontalSliderWIthTitle(
-              title: "Top Rated Movies",
-            ),
-            const HorizontalSliderWIthTitle(
-              title: "Upcoming Movies",
-            ),
-            SizedBox(height: .05 * screenHeight),
-          ],
-        ),
+          );
+        }),
       ),
     );
   }
@@ -138,6 +157,7 @@ class _HomeScreenState extends State<HomeScreen> {
       (value) {
         setState(() {
           trending.addAll(value!.results!);
+          isLoading = false;
         });
       },
     ).onError(
