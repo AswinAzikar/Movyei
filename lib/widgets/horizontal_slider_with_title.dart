612b708ebc27446b0fd76a8461cc81b159157db2
AswinAzikar <azwinaswin8@gmail.com>
2024-07-13 15:23:32 +0530

    First Stable verison: "Deleted DetailScreen_carousel.dart file and updated HorizontalSliderWIthTitle.dart file"

--- lib/widgets/horizontal_slider_with_title.dart
+++ lib/widgets/horizontal_slider_with_title.dart
@@ -2,6 +2,8 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+import 'package:moviyee/Screens/DetailScreen.dart';
+import 'package:moviyee/Screens/DetailScreen_carousel.dart';
 
 import '../controllers/Api/remote_data_sorce.dart';
 import '../models/movie_model.dart';
@@ -35,14 +37,12 @@ class _HorizontalSliderWIthTitleState extends State<HorizontalSliderWIthTitle> {
     final String sectionName;
     if (widget.title == 'Upcoming Movies') {
       sectionName = "upcoming";
-    } else if(widget.title == "Top Rated"){
+    } else if (widget.title == "Top Rated") {
       sectionName = "top_rated";
-    }
-    else{
+    } else {
       sectionName = "now_playing";
     }
-    final items = await RemoteDataSorce.fetchMovies(
-        pageKey, sectionName);
+    final items = await RemoteDataSorce.fetchMovies(pageKey, sectionName);
     if (items.length < 20) {
       _pagingController.appendLastPage(items);
     } else {
@@ -59,7 +59,8 @@ class _HorizontalSliderWIthTitleState extends State<HorizontalSliderWIthTitle> {
           padding: const EdgeInsets.all(20.0),
           child: Text(
             widget.title,
-            style: GoogleFonts.abel(fontSize: 24),
+            style: TextStyle(
+                fontFamily: GoogleFonts.montserrat().fontFamily, fontSize: 22),
           ),
         ),
         SizedBox(height: 10),
@@ -72,28 +73,36 @@ class _HorizontalSliderWIthTitleState extends State<HorizontalSliderWIthTitle> {
               itemBuilder: (context, item, index) {
                 final imageUrl =
                     'https://image.tmdb.org/t/p/w500${item.posterPath}';
-                return AspectRatio(
-                  aspectRatio: .7,
-                  child: Column(
-                    children: [
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(18),
-                            child: CachedNetworkImage(
-                              imageUrl: imageUrl,
-                              fit: BoxFit.cover,
-                              placeholder: (context, url) =>
-                                  Center(child: CircularProgressIndicator()),
-                              errorWidget: (context, url, error) =>
-                                  Icon(Icons.error),
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => DetailScreen(result: item)));
+                  },
+                  child: AspectRatio(
+                    aspectRatio: .7,
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(18),
+                              child: CachedNetworkImage(
+                                imageUrl: imageUrl,
+                                fit: BoxFit.cover,
+                                placeholder: (context, url) =>
+                                    Center(child: CircularProgressIndicator()),
+                                errorWidget: (context, url, error) =>
+                                    Icon(Icons.error),
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      Text(item.title ?? "")
-                    ],
+                        Text(item.title ?? "")
+                      ],
+                    ),
                   ),
                 );
               },
